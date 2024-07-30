12c6c0cfc4f593ec50d357a9232010937e7dafd0
ctgadget <chaance.graves@ctginnovations.io>
2024-07-30 16:31:26 +0000

    updates for assignment 3 pt. 1

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -49,9 +49,9 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
+echo "Removing the old writer utility and compiling as a native application"
 make clean
-make
+make 
 
 for i in $( seq 1 $NUMFILES)
 do

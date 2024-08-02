52e0860ba22f5ec9d562d38a4b499eeeb5e30fc9
ctgadget <chaance.graves@ctginnovations.io>
2024-08-02 18:41:05 +0000

    updated manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
+# Modified for Assignment 3 part 2
 
 set -e
 set -u
+
+# Define colors
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
@@ -35,11 +41,19 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+
+    # clean up - use with caution
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    # build steps
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
-echo "Adding the Image in outdir"
+echo -e "${YELLOW}Adding the Image in outdir${NC}"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
-echo "Creating the staging directory for the root filesystem"
+echo -e "${YELLOW}Creating the staging directory for the root filesystem${NC}"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
@@ -48,6 +62,10 @@ then
 fi
 
 # TODO: Create necessary base directories
+echo -e "${YELLOW}Create necessary base directories${NC}"
+mkdir -p ${OUTDIR}/rootfs/{bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,usr,var}
+mkdir -p ${OUTDIR}/rootfs/usr/{bin,lib,sbin}
+mkdir -p ${OUTDIR}/rootfs/var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +74,91 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+echo -e "${YELLOW}make and install busybox${NC}"
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+if [ ! -e ${OUTDIR}/rootfs/bin/busybox ]; then
+  echo "BusyBox binary not found"
+  exit 1
+fi
 
-# TODO: Add library dependencies to rootfs
+# busybox binary
+# setuid root to ensure all configured applets will
+# work properly
+sudo chmod 4755 ${OUTDIR}/rootfs/bin/busybox
+
+echo -e "${YELLOW}Library dependencies${NC}"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
+
+# Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+LINKER=$(${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter" | awk -F ': ' '{print $2}' | tr -d ']')
+SHARED_LIBS=$(${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library" | awk '{print $5}' | tr -d '[]' | tr '\n' ' ')
+
+# Create necessary directories for libraries
+mkdir -p ${OUTDIR}/rootfs/lib
+mkdir -p ${OUTDIR}/rootfs/lib64
+
+# Copy dynamic linker/loader to rootfs
+cp ${SYSROOT}${LINKER} ${OUTDIR}/rootfs${LINKER}
+
+# Function to copy libraries if they exist
+copy_library() {
+    LIB=$1
+    if [ -f ${SYSROOT}/lib/${LIB} ]; then
+        cp ${SYSROOT}/lib/${LIB} ${OUTDIR}/rootfs/lib/
+    elif [ -f ${SYSROOT}/lib64/${LIB} ]; then
+        cp ${SYSROOT}/lib64/${LIB} ${OUTDIR}/rootfs/lib64/
+    else
+        echo "Warning: ${LIB} not found in sysroot."
+    fi
+}
+
+# Copy shared libraries to rootfs
+for LIB in ${SHARED_LIBS}; do
+    copy_library ${LIB}
+done
 
 # TODO: Make device nodes
+echo -e "${YELLOW}make device nodes${NC}"
+
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+echo "${YELLOW}Clean and build the writer utility${NC}"
+
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo -e "${YELLOW}Copy the finder related scripts and executables to the /home directory${NC}"
+
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp -Lr ${FINDER_APP_DIR}/conf/ ${OUTDIR}/rootfs/home/
 
 # TODO: Chown the root directory
+echo -e "${YELLOW}Chown the root directory${NC}"
+
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+echo -e "${YELLOW}Create initramfs.cpio.gz${NC}"
+
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
